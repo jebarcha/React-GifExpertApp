f6393b88e6a48bb522d425835918aadc32d5e18a
jebarcha <jebarcha@hotmail.com>
2020-06-30 18:05:47 -0500

    docs agregado

---
+++ docs/precache-manifest.bf938ae6060ccc6aa11d0de2f333fc88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1ce7a995dbfce2175001bc6010d2f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48113f399fa44cf41ecd",
+    "url": "/static/css/main.305e5aae.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48113f399fa44cf41ecd",
+    "url": "/static/js/main.df88552c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
